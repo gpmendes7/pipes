d7049bdc83cd04b418594455147009443b38cfa6
Gabriel Pereira Mendes <gabriel.mendes@montreal.com.br>
2024-10-22 10:45:49 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  pessoa = {
+    name: 'Felipe',
+    status: 1,
+  }
+
+  pessoa2 = {
+    name: 'Matheus',
+    status: 2,
+  }
+
+  pessoa3 = {
+    name: 'Luis',
+    status: 3,
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatusClassPipe } from './pipes/status.class.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusClassPipe
   ],
   imports: [
     BrowserModule
